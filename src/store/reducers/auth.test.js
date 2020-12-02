2767e27b680018fe05aaddf956bfe2920de89260
nikhilmohan <nikmohan81@gmail.com>
2020-12-02 23:19:04 +0530

    updated tests

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -5,22 +5,22 @@ describe ('AuthReducer ', () => {
   it('should have initial state set by reducer', () => {
     expect(reducer(undefined, {})).toEqual({
       token : null,
-      email : null,
+      id : null,
       error: null
     });
   });
-  it('should have token and email updated in state after auth success', () => {
+  it('should have token and id updated in state after auth success', () => {
     expect(reducer({
       token : null,
-      email : null,
+      id : null,
       error: null
       }, {
         type : actionTypes.AUTH_SUCCESS,
         token : 'some-token',
-        email : 'test@ab.com'
+        id : 'some-id'
       })).toEqual({
         token : 'some-token',
-        email : 'test@ab.com',
+        id : 'some-id',
         error: null
     });
   });
